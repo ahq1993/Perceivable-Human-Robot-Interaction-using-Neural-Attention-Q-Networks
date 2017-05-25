0af647c14616fe047f8dc1f9fed3874c2fdc7369
Ahmed Hussain Qureshi <ahqureshi1993@gmail.com>
2017-05-25 13:19:04 +0900

    Update CNN.lua

--- DataGeneration-phase/CNN.lua
+++ DataGeneration-phase/CNN.lua
@@ -17,7 +17,6 @@ function CNN.cnn(args)
 	
 
 	modelA=nn.Sequential()
-	--network:add(nn.Reshape(unpack(args.input_dims)))
 	--cov1
 	modelA:add(nn.SpatialConvolution(nfeats, nstates[1],filter[1],filter[1],stride[1],stride[1],1))
 	modelA:add(nn.ReLU())
@@ -37,50 +36,26 @@ function CNN.cnn(args)
 	    
     local last_num_features = nstates[4]
 
-    -- Add convolutional layers
-   
-
-
     local n_hiddens = last_num_features
 
     local x = nn.Identity()()
     local x1 = nn.Identity()(modelA(x))
     local x2 = nn.Reshape(last_num_features, 49)(x1)
-    -- batch (32) x features (64) x annotations (49)
     local x3 = nn.SpatialConvolution(last_num_features, n_hiddens, 1, 1, 1, 1)(x1)
-    -- batch (32) x hiddens (42) x height (7) x width (7)
-
     local h = nn.Identity()()
     local h1 = nn.LinearWithoutBias(args.rnn_size, n_hiddens)(h)
-			 h1=h1:float()
-    -- batch (32) x hiddens (42)
+    h1=h1:float()
     local h2 = nn.Replicate(49, 3)(h1)
-    -- batch (32) x hiddens (42) x annotations (49)
     local h3 = nn.Reshape(n_hiddens, 7, 7)(h2)
-    -- batch (32) x hiddens (42) x height (7) x width (7)
-
     local a1 = nn.Tanh()(nn.CAddTable()({h3, x3}))
     local a2 = nn.SpatialConvolution(n_hiddens, 1, 1, 1, 1, 1)(a1)
-    -- batch (32) x softmax (1) x height (7) x width (7)
     local a3 = nn.SoftMax()(nn.Reshape(49)(a2))
-    -- batch (32) x annotations (49)
-    
     local a4 = nn.Replicate(last_num_features, 2)(a3)
-    -- batch (32) x features (64) x annotations (49)
-
     local context = nn.Sum(3)(nn.CMulTable()({a4, x2}))
-    -- batch (32) x features (64)
-
     local g = nn.gModule({h,x}, {a3,context})
 
-    
-		 g:float()
+    g:float()
 
-
-    --if args.verbose >= 100 then
-        --graph.dot(g.fg, 'Forward Graph', 'fg')
-        --graph.dot(g.bg, 'Backward Graph', 'bg')
-    --end
 
     return g
 end
